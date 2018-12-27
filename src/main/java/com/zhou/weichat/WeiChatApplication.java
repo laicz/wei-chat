ad572b89d85249b3c9443d798befe4bd01eacb47
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-12-27 22:48:56 +0800

    Initial commit

---
+++ src/main/java/com/zhou/weichat/WeiChatApplication.java
@@ -0,0 +1,14 @@
+package com.zhou.weichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiChatApplication.class, args);
+    }
+
+}
+
